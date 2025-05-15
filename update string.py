95673ddd1eb056d8a9fbde5994c6756ecbcacb06
Nurahmanlatif <nurahmanlatif99@gmail.com>
2025-05-15 14:59:07 +0700

    tugas 7

---
+++ update string.py
@@ -0,0 +1,10 @@
+nama = 'nurahman'
+print('Sebelum =', nama)
+
+# Menyisipkan nama "Latif" di tengah nama
+nama_baru = nama + ' Latif ' + nama
+print('Sesudah =', nama_baru)
+
+# Menukar posisi kata belakang dan depan, serta menambahkan "Latif"
+hasil = nama + ' ' + nama + ' Latif'
+print(hasil)
