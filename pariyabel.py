5d403c756372a0bd1394e7c89808cd3de9189b27
Nurahmanlatif <nurahmanlatif99@gmail.com>
2025-05-15 14:56:14 +0700

    tugas 3

---
+++ pariyabel.py
@@ -0,0 +1,30 @@
+# proses memasukkan data ke dalam variabel
+kota = "Jakarta"
+# proses mencetak variabel
+print(kota)
+
+# nilai dan tipe data dalam variabel dapat diubah
+tinggi = 160             # nilai awal
+print(tinggi)            # mencetak nilai tinggi
+print(type(tinggi))      # mengecek tipe data tinggi
+tinggi = "seratus enam puluh"  # nilai setelah diubah
+print(tinggi)            # mencetak nilai tinggi
+print(type(tinggi))      # mengecek tipe data tinggi
+
+merkMobil = "Toyota"
+tipeMobil = "Avanza"
+mobil = merkMobil + " " + tipeMobil
+tahun = 2020
+warna = "Hitam"
+print("Info Mobil:\n", mobil, "\n", tahun, "\n", warna)
+
+# contoh variabel lainnya
+contoh1 = "Selamat"
+contoh_dua = "Datang"
+_contohTiga = "Di"
+contoh4 = "Python"
+
+panjang_ruangan = 8
+lebar_ruangan = 4
+luas_ruangan = panjang_ruangan * lebar_ruangan
+print(luas_ruangan)
