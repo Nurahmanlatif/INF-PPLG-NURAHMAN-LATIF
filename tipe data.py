2b36abcd75c65dc1150601891009a943a7a14696
Nurahmanlatif <nurahmanlatif99@gmail.com>
2025-05-15 14:55:40 +0700

    tugas 2

---
+++ tipe data.py
@@ -0,0 +1,39 @@
+# Tipe data Boolean
+print(False)
+
+# Tipe data String
+print("Belajar Python itu seru")
+print('Python bisa dipakai untuk banyak hal')
+
+# Tipe data Integer
+print(100)
+
+# Tipe data Float
+print(2.718)
+
+# Tipe data Hexadecimal
+print("Bentuk hexadecimal dari 45 adalah " + hex(45))
+
+# Gabungan String
+print("Nama saya : " + "Lina")
+
+# Tipe data Complex
+print(7 + 2j)
+
+# Tipe data List
+print([10, 20, 30, 40])
+print(["apel", "jeruk", "mangga"])
+
+# Tipe data Tuple
+print((6, 7, 8, 9))
+print(("alpha", "beta", "gamma"))
+
+# Tipe data Dictionary
+print({"kelas": "XI IPA 1", "tahun": 2025})
+
+# Dictionary dalam variabel
+data_siswa = {"nama": "Sari", "umur": 19}
+print(data_siswa)
+(type(data_siswa))  # Akan tampil <class 'dict'>print
+
+
