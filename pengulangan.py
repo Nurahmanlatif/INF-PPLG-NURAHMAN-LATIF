b18228bfae978cb4a35c3b696789ed65962641fa
Nurahmanlatif <nurahmanlatif99@gmail.com>
2025-05-15 14:58:15 +0700

    tugas 5

---
+++ pengulangan.py
@@ -0,0 +1,13 @@
+# Contoh pengulangan for sederhana dengan nama Latif
+angka = [10, 20, 30, 40, 50]
+for x in angka:
+    print("Latif memilih angka", x)
+
+# Contoh pengulangan for dengan daftar makanan
+makanan = ["bakso", "sate", "nasi goreng"]
+for item in makanan:
+    print("Latif suka makan", item)
+
+# Pengulangan dengan range dan nama Latif
+for i in range(5, 11):
+    print("Latif sedang menghitung:", i)
