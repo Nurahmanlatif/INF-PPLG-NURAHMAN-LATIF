4a7ba9ab5367ea847b096d905a938a38a3a77142
Nurahmanlatif <nurahmanlatif99@gmail.com>
2025-05-15 14:54:00 +0700

    tugas 1

---
+++ komentar.py
@@ -0,0 +1,29 @@
+# Ini adalah baris komentar
+# Komentar ini tidak akan dijalankan oleh Python
+
+# Komentar dengan tanda '#' hanya berlaku
+# untuk
+# satu
+# baris
+
+"""
+Komentar lebih dari satu baris
+ditulis menggunakan tiga tanda kutip ganda
+dan diakhiri dengan tiga kutip ganda
+"""
+
+print("Halo Dunia") # Ini juga komentar di akhir baris
+
+"""
+print("Ini tidak akan dijalankan karena ada di dalam komentar multiline")
+"""
+
+# print("Selamat Datang")
+
+# Komentar juga dapat mengandung karakter khusus seperti: ~`<>?:{}|=-+_
+
+# Menampilkan nama
+print("Rapael cinta della")
+
+# Menampilkan bilangan bulat
+print(456)
