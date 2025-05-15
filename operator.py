180267d387eb17d97dc3c6fbdc5f60f160cf882e
Nurahmanlatif <nurahmanlatif99@gmail.com>
2025-05-15 14:57:15 +0700

    tugas 4

---
+++ operator.py
@@ -0,0 +1,42 @@
+# OPERATOR ARITMATIKA
+
+# Penjumlahan
+print(20 + 5)
+pisang = 10
+mangga = 12
+totalBuah = pisang + mangga  # Menjumlahkan jumlah buah
+print(totalBuah)
+
+# Pengurangan
+tagihan = 150000
+uangDibayar = 60000
+sisaTagihan = tagihan - uangDibayar
+print("Sisa tagihan Anda sebesar", sisaTagihan)
+
+# Perkalian
+alas = 10
+tinggi = 6
+luasSegitiga = (alas * tinggi) / 2
+print(luasSegitiga)
+
+# Pembagian
+donat = 24
+teman = 6
+donatPerTeman = donat / teman
+print("Setiap teman akan mendapatkan", donatPerTeman, "donat")
+
+# Sisa Bagi / Modulus
+angka1 = 27
+angka2 = 4
+sisa = angka1 % angka2
+print("Sisa pembagian dari", angka1, "dengan", angka2, "adalah", sisa)
+
+# Pangkat
+angka3 = 5
+angka4 = 3
+hasilEksponen = angka3 ** angka4
+print(hasilEksponen)
+
+# Pembagian Bulat
+print(25 // 4)
+# 25 dibagi 4 adalah 6.25. Karena pembagian bulat, maka hasilnya adalah 6
